26eda1ff4f055519ee497b7d4b2b534e8c458e16
Iksal Fauzan Gilman <iksalfauzangilman@gmail.com>
2025-02-07 16:24:30 +0700

    menyelesaikan halaman produk

--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -30,6 +30,35 @@
             background-color: #5DA9FF;
             z-index: 0;
         }
+
+        .step-number2 {
+            width: 40px;
+            height: 40px;
+            background-color: #8854BB;
+            color: white;
+            border-radius: 50%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-weight: bold;
+            margin: 0 auto 1rem;
+            position: relative;
+            z-index: 1;
+        }
+
+        .step-container2 {
+            position: relative;
+        }
+
+        .step-connector2 {
+            position: absolute;
+            top: 20px;
+            left: 25%;
+            right: 25%;
+            height: 4px;
+            background-color: #8854BB;
+            z-index: 0;
+        }
     </style>
     {{-- konten pertama --}}
     <div class="bg-light w-100 d-flex justify-content-around flex-column align-items-center">
@@ -42,14 +71,14 @@
                 <span>Pembuat vote bisa menambahkan kode room </span>
             </div>
             <div class="card bawah shadow p-5" style="width: 25rem; height: 14rem; border-radius: 20px;">
-                <i class="fa-solid fa-lock fs-1 mb-4" style="color: #72B5F6"></i>
-                <span class="fw-bold fs-5">Keamanan Voting</span>
-                <span>Pembuat vote bisa menambahkan kode room </span>
+                <i class="fa-solid fa-clock fs-1 mb-4" style="color: #72B5F6"></i>
+                <span class="fw-bold fs-5">Kurun Waktu</span>
+                <span>Dengan adanya fitur ini pembuat voting bisa membatasi waktu voting</span>
             </div>
             <div class="card kanan shadow p-5" style="width: 25rem; height: 14rem; border-radius: 20px;">
-                <i class="fa-solid fa-lock fs-1 mb-4" style="color: #72B5F6"></i>
-                <span class="fw-bold fs-5">Keamanan Voting</span>
-                <span>Pembuat vote bisa menambahkan kode room </span>
+                <i class="fa-solid fa-filter fs-1 mb-4" style="color: #72B5F6"></i>
+                <span class="fw-bold fs-5">Filterisasi Vote</span>
+                <span>Fitur ini memudahkan pembuat vote menyeleksi vote</span>
             </div>
         </div>
     </div>
@@ -87,4 +116,64 @@
         </div>
     </div>
     {{-- end konten kedua --}}
+
+    {{-- konten ketiga --}}
+    <div class="bg-light w-100 d-flex justify-content-around flex-column align-items-center">
+        <span class="fs-2 fw-bold  span-title"
+            style="color: #8854BB; margin-bottom: 20px; margin-top: 140px;">Penjadwalan</span>
+        <span class="mb-5 fs-4 fw-bold span-sub" style="color: #000;">Keunggulan Produk Kami</span>
+        <div class="d-flex flex-row justify-content-evenly w-100" style="margin-bottom: 7rem">
+            <div class="card kiri shadow p-5" style="width: 25rem; height: 14rem; border-radius: 20px;">
+                <i class="fa-solid fa-users fs-1 mb-4" style="color: #8854BB"></i>
+                <span class="fw-bold fs-5">Pembuatan Group</span>
+                <span>Fitur ini memudahkan komunikasi antara pembuat jadwal dan anggota</span>
+            </div>
+            <div class="card bawah shadow p-5" style="width: 25rem; height: 14rem; border-radius: 20px;">
+                <i class="fa-solid fa-gears  fs-1 mb-4" style="color: #8854BB"></i>
+                <span class="fw-bold fs-5">Flexibelitas</span>
+                <span>Pembuat jadwal bisa menentukan jadwal mulai dari jam</span>
+            </div>
+            <div class="card kanan shadow p-5" style="width: 25rem; height: 14rem; border-radius: 20px;">
+                <i class="fa-solid fa-check-to-slot fs-1 mb-4" style="color: #8854BB"></i>
+                <span class="fw-bold fs-5">Konfirmasi Cepat</span>
+                <span>Pembuat jadwal bisa membuat pertemuan tanpa menunggu</span>
+            </div>
+        </div>
+    </div>
+    {{-- end konten ketiga --}}
+
+    {{-- konten keempat --}}
+    <div class="w-100 d-flex justify-content-around flex-column align-items-center" style="background-color: #F7EFFF">
+        <span class="mb-5 mt-5 fs-4 fw-bold span-sub" style="color: #000;">Panduan Voting</span>
+        <div class="card bawah shadow mb-5 p-5" style="width: 90%; border-radius: 20px; ">
+            <div class="head d-flex justify-content-around" style="color: #8854BB">
+                <p class="fw-bold">Membuat Penjadwalan</p>
+                <p class="fw-bold">Memilih Jadwal</p>
+            </div>
+            <div class="maincard d-flex justify-content-around mt-5 mb-3">
+                <div class="card kiri bg-secondary" style="border-radius: 20px; width: 35%; height: 13rem;"></div>
+                <div class="card kanan bg-secondary" style="border-radius: 20px; width: 35%; height: 13rem;"></div>
+            </div>
+            <div class="step-container2">
+                <div class="step-connector2"></div>
+                <div class="row text-center">
+                    <div class="step-number2">1</div>
+                    {{-- <div class="col-md-6">
+                        <p>Masukkan terlebih dahulu kode room yang diberikan dengan link</p>
+                    </div> --}}
+                    <div class="step-number2">2</div>
+                    {{-- <div class="col-md-6">
+                        <p>Pilih pilihan voting anda lalu klik tombol simpan</p>
+                    </div> --}}
+                </div>
+            </div>
+            <div class="text d-flex justify-content-around text-center flex-row">
+                <p class="bawah" style="width: 35%;">Pengguna membuat grup terlebih dahulu</p>
+                <p class="bawah" style="width: 35%;">Lalu pilih jadwal yang tersedia, lalu konfirmasikan</p>
+            </div>
+        </div>
+    </div>
+    {{-- end konten keempat --}}
+    {{-- footer --}}
+    @include('layout.footer')
 @endsection
