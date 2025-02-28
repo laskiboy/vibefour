5c619d4a9aab95702aadcdd58094afcc56534264
Iksal Fauzan Gilman <iksalfauzangilman@gmail.com>
2025-02-28 16:45:07 +0700

    membuat model baru untuk tabel otp_codes

---
+++ app/Models/OtpCode.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OtpCode extends Model
+{
+    use HasFactory;
+    protected $fillable = ['user_id', 'otp', 'expires_at', 'attempts'];
+    protected $dates = ['expires_at'];
+
+    public function isExpired()
+    {
+        return Carbon::now()->gt($this->expires_at);
+    }
+}
